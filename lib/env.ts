d30d55d61fb2d59aa8a8a3dfe9f7b0e44849cf38
simpletools101 <simpletools@hotmail.com>
2025-09-23 18:57:36 +0300

    soriana

--- lib/env.ts
+++ lib/env.ts
@@ -1,13 +1,9 @@
 function setURL() {
-    let uri = 'http://localhost:3000'
-    if (process.env.NODE_ENV == 'development') {
-        return uri
-    } else {
-        return 'https://goheza-app.vercel.app'
-    }
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:3000'
+  } else {
+    return process.env.NEXT_PUBLIC_SITE_URL || 'https://goheza-app.vercel.app'
+  }
 }
-
-console.log("current-environment",process.env.NODE_ENV)
-
 
 export const baseURL = setURL()
