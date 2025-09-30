76f66861a930a2ffc9e08580731b3e18da397fb6
simpletools101 <simpletools@hotmail.com>
2025-09-30 19:12:24 +0300

    make leaving tonight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx (server component)
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import { Lexend } from 'next/font/google'
 import { Toaster } from '@/components/ui/sonner'
 import Header from '@/components/layout/header'
 import './globals.css'
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
     description: 'Connecting Brands and Creators',
 }
 
-const sans = Roboto({ subsets: ['latin'], preload: true })
+const Lexendfont = Lexend({
+    subsets: ['latin'],
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
-
     return (
         <html lang="en">
-            <body className={sans.className}>
+            <body className={Lexendfont.className}>
                 <Header /> {/* <-- client component */}
                 {children}
                 <Toaster />
