e9d556c468d4dcef9f2b87912507990127b6ade6
simpletools101 <simpletools@hotmail.com>
2026-02-24 00:44:35 +0300

    simon-says

--- components/components/landing/footertc.tsx
+++ components/components/landing/footertc.tsx
@@ -139,7 +139,7 @@ export default function FooterComponent() {
                     </div>
 
                     <div className="border-t border-gray-700 mt-10 pt-8 text-center text-gray-400 text-sm">
-                        <p>&copy; {new Date().getFullYear()} Goheza. All rights reserved.</p>
+                        <p>&copy; {new Date().getFullYear()} Goheza Technologies Company Limited . All rights reserved.</p>
                     </div>
                 </div>
             </footer>
