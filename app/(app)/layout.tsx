41481a3b835463f769b524dd661a350c9c02886d
simpletools101 <simpletools@hotmail.com>
2026-05-18 20:51:48 +0300

    welel

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import local from 'next/font/local'
+import { Toaster } from 'sonner'
 
 const Lexendfont = local({
     src: './fonts/lexend.ttf',
@@ -17,7 +18,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={Lexendfont.className}>
-            <body>{children}</body>
+            <body>
+                {children}
+                <Toaster richColors position="bottom-right" />
+            </body>
         </html>
     )
 }
